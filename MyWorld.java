01521894b4d399b55ea7f0a235f6a2f797caa4ac
Angel Herrarte <angelherrarte3@gmail.com>
2022-08-10 17:00:20 -0600

    Agregar bicho al iniciar juego

--- MyWorld.java
+++ MyWorld.java
@@ -1,22 +1,21 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class MyWorld extends World
-{
+public class MyWorld extends World {
     /**
      * Constructor de la clase MyWorld
      * 
      */
-    public MyWorld()
-    {    
+    public MyWorld() {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         MusicPlayer musicPlayer = new MusicPlayer("track_1.mp3");
         //musicPlayer.play(true);
+        addObject(new Bug(), Greenfoot.getRandomNumber(550), Greenfoot.getRandomNumber(350));
     }
 }
